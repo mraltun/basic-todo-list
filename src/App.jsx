69b92634dfe7a92dea0db729c3c7bf562f66990b
Murat Altun <mraltun@gmail.com>
2022-08-05 13:27:06 +0300

    Added Alert and Todos components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from "react";
+import Alert from "./components/Alert";
+import Todos from "./components/Todos";
 
 const App = () => {
   const [name, setName] = useState("");
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
-  const [alert, setAlert] = useState({});
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
-  return <div>App</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted!");
+  };
+
+  return (
+    <section className='section-center'>
+      <form className='todo-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+      </form>
+      <div className='todo-container'>
+        <Todos />
+        <button className='clear-btn'>Clear Items</button>
+      </div>
+    </section>
+  );
 };
 
 export default App;
