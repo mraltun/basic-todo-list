00df7445af2d9ab24976d58981a53ab7ff582703
Murat Altun <mraltun@gmail.com>
2022-08-05 14:11:00 +0300

    Updated Todos component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Alert from "./components/Alert";
 import Todos from "./components/Todos";
+import "./App.css";
 
 const App = () => {
   const [name, setName] = useState("");
@@ -11,16 +12,42 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form Submitted!");
+    if (!name) {
+      // Display alert
+    } else if (name && isEditing) {
+      // Edit here
+    } else {
+      // Show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  const handleChange = (e) => {
+    setName(e.target.value);
   };
 
   return (
     <section className='section-center'>
       <form className='todo-form' onSubmit={handleSubmit}>
         {alert.show && <Alert />}
+        <h3>Basic Todo List</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='todo'
+            placeholder='What are you going to do?'
+            value={name}
+            onChange={handleChange}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? "Edit" : "Submit"}
+          </button>
+        </div>
       </form>
       <div className='todo-container'>
-        <Todos />
+        <Todos items={list} />
         <button className='clear-btn'>Clear Items</button>
       </div>
     </section>
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
-const Todos = () => {
-  return <div>Todos</div>;
+const Todos = ({ items }) => {
+  return (
+    <div className='todo-list'>
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className='todo-item'>
+            <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <button type='button' className='edit-btn'>
+                <FaEdit />
+              </button>
+              <button type='button' className='delete-btn'>
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Todos;
